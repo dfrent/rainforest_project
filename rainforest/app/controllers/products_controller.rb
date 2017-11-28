52f95aa6b48c73418e8639d032c69544aacd59b5
somethinrother <jamesneilson4@gmail.com>
2017-11-28 12:02:06 -0500

    Add view, routes, and behaviour for new_product page

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,11 +1,32 @@
 class ProductsController < ApplicationController
 
-def index
-  @products = Product.all
-end
+  def index
+    @products = Product.all
+  end
 
-def show
-  @product = Product.find(params[:id])
-end
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.price = params[:product][:price]
+    @product.description = params[:product][:description]
+
+    if @product.save
+      redirect_to products_url
+    else
+      redirect_to new_products_url
+    end
+  end
+
+  def update
+
+  end
 
 end
