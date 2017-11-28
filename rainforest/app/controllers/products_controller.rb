285361233c3f7cf152c8b8a6d78fee26ff1c1312
cordell <cordell.perry@gmail.com>
2017-11-28 11:36:10 -0500

    [Rainforest] product seeds show page

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -4,4 +4,8 @@ def index
   @products = Product.all
 end
 
+def show
+  @product = Product.find(params[:id])
+end
+
 end
--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create name: 'water bottle', description: 'holds various liquids', price: 50000
+
+Product.create name: 'ping pong balls', description:'fun for the whole family', price: 60000
+
+Product.create name: 'pen & paper bundle', description: 'allows for various forms for expression', price: 900000
